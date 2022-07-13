1118547ffe8ad3238f03fec47d8e2238f13fae23
Toxa <untoxa@mail.ru>
2022-07-13 11:00:49 +0300

    switching of modes and display them (temporary) on the top of the camera screen

--- include/state_camera.h
+++ include/state_camera.h
@@ -8,4 +8,16 @@ typedef enum {
     camera_mode_iterate  
 } camera_mode_e;
 
+typedef enum {
+    trigger_mode_abutton,
+    trigger_mode_timer,
+    trigger_mode_interval
+} trigger_mode_e;
+
+typedef enum {
+    after_action_save,
+    after_action_print,
+    after_action_printsave
+} after_action_e;
+
 #endif
--- src/state_camera.c
+++ src/state_camera.c
@@ -22,12 +22,18 @@
 BANKREF(state_camera)
 
 camera_mode_e camera_mode = camera_mode_manual;
+trigger_mode_e trigger_mode = trigger_mode_abutton;
+after_action_e after_action = after_action_save;
 
-const uint8_t * const modes[] = {" Manual", " Assist", " Auto", " Iterate"};
+const uint8_t * const camera_modes[] = {" Manual", " Assist", " Auto", " Iterate"};
+const uint8_t * const trigger_modes[] = {" Btn: [A]", " Timer", " Interval"};
+const uint8_t * const after_actions[] = {" Save", " Print", " S & P"};
 
 static void refresh_screen() {
     screen_clear_rect(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET, DEVICE_SCREEN_WIDTH, DEVICE_SCREEN_HEIGHT, SOLID_BLACK);
-    menu_text_out(0, 0, 5, SOLID_WHITE, modes[camera_mode]);
+    menu_text_out( 0, 0, 5, SOLID_WHITE, camera_modes[camera_mode]);
+    menu_text_out( 5, 0, 5, SOLID_WHITE, trigger_modes[trigger_mode]);
+    menu_text_out(10, 0, 5, SOLID_WHITE, after_actions[after_action]);
 }
 
 uint8_t ENTER_state_camera() BANKED {
@@ -56,11 +62,22 @@ uint8_t UPDATE_state_camera() BANKED {
                 camera_mode = camera_mode_iterate;
                 break;            
             case ACTION_TRIGGER_ABUTTON:
+                trigger_mode = trigger_mode_abutton;
+                break;
             case ACTION_TRIGGER_TIMER:
+                trigger_mode = trigger_mode_timer;
+                break;
             case ACTION_TRIGGER_INTERVAL:
+                trigger_mode = trigger_mode_interval;
+                break;
             case ACTION_ACTION_SAVE:
+                after_action = after_action_save;
+                break;
             case ACTION_ACTION_PRINT:
+                after_action = after_action_print;
+                break;
             case ACTION_ACTION_SAVEPRINT:
+                after_action = after_action_printsave;
                 break;
             default:
                 // error, must not get here
