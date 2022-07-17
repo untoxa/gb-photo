7e8b24954eea3a0adf1abbee03c9a9ed8cc08f9f
Toxa <untoxa@mail.ru>
2022-07-17 12:37:46 +0300

    fix missing save_camera_state() declaration

--- include/load_save.h
+++ include/load_save.h
@@ -6,5 +6,6 @@
 void init_save_structure() BANKED;
 void save_camera_mode_settings(camera_mode_e mode) BANKED;
 void restore_default_mode_settings(camera_mode_e mode) BANKED;
+void save_camera_state() BANKED;
 
 #endif
