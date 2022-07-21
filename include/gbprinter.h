5770e886f3ab7207225c4a10314efa79918082cc
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2022-07-21 14:44:52 +0200

    Update gbprinter.h

--- include/gbprinter.h
+++ include/gbprinter.h
@@ -10,30 +10,31 @@
 #define PRN_LOW(A) ((A) & 0xFF)
 #define PRN_HIGH(A) ((A) >> 8)
 
-#define PRN_MAGIC               0x3388
+#define PRN_MAGIC               0x3388 //Mandatory first bytes to initialised a communication with printer
 #define PRN_MAGIC_1             PRN_LOW(PRN_MAGIC)
 #define PRN_MAGIC_2             PRN_HIGH(PRN_MAGIC)
+// Two consecutive linked commands must never be more than 150 ms apart
+#define PRN_CMD_INIT            0x01 //init command must be the first one received by the printer. It is valid for at least 10 seconds
+#define PRN_CMD_PRINT           0x02 //print command
+#define PRN_CMD_DATA            0x04 //data command. Can be any length between 0 and 640 bytes. Length 0 triggers PRN_STATUS_FULL and is mandatory before print command
+#define PRN_CMD_ABORT           0x08 //abort command
+#define PRN_CMD_STATUS          0x0F //status commnd. Resets PRN_STATUS_UNTRAN
 
-#define PRN_CMD_INIT            0x01
-#define PRN_CMD_PRINT           0x02
-#define PRN_CMD_DATA            0x04
-#define PRN_CMD_STATUS          0x0F
-
-#define PRN_PALETTE_NORMAL      0b11100100u
+#define PRN_PALETTE_NORMAL      0b11100100u // 0b00000000u acts the same for the printer
 #define PRN_PALETTE_INV         0b00011011u
 
 #define PRN_EXPOSURE_LIGHT      0x00
-#define PRN_EXPOSURE_DEFAULT    0x40
-#define PRN_EXPOSURE_DARK       0x7F
+#define PRN_EXPOSURE_DEFAULT    0x40 //default value in most games
+#define PRN_EXPOSURE_DARK       0x7F //values from 0x80 to 0xFF act as 0x40
 
-#define PRN_STATUS_LOWBAT       0x80
-#define PRN_STATUS_ER2          0x40
-#define PRN_STATUS_ER1          0x20
-#define PRN_STATUS_ER0          0x10
-#define PRN_STATUS_UNTRAN       0x08
-#define PRN_STATUS_FULL         0x04
-#define PRN_STATUS_BUSY         0x02
-#define PRN_STATUS_SUM          0x01
+#define PRN_STATUS_LOWBAT       0x80 //battery too low
+#define PRN_STATUS_ER2          0x40 //unspecified error
+#define PRN_STATUS_ER1          0x20 //paper jam
+#define PRN_STATUS_ER0          0x10 //packet error
+#define PRN_STATUS_UNTRAN       0x08 //allows to verify that printer got data with correct checksum
+#define PRN_STATUS_FULL         0x04 //status data ready, mandatory to allow printing
+#define PRN_STATUS_BUSY         0x02 //message while physically printing
+#define PRN_STATUS_SUM          0x01 //checksum error
 #define PRN_STATUS_OK           0x00
 
 #define PRN_STATUS_MASK_ERRORS  0xF0
