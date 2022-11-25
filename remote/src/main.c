1ee44ceefb1c8c40d32b36b451f86473fb3d0a41
Roger Braunstein <roger@partlyhuman.com>
2022-11-24 16:53:30 -0800

    Allow changing link connection state while remote in use

--- remote/src/main.c
+++ remote/src/main.c
@@ -5,20 +5,15 @@
 #include "remote.h"
 #include "remote_down.h"
 
-inline void send_data(uint8_t b) {
+#define LINK_CYCLES_MAX 0x200
+
+// If link cable disconnected, return false
+inline uint8_t send_data(uint8_t b) {
+    uint16_t guard = 0;
     SB_REG = b;
     SC_REG = 0x81;
-    while (SC_REG & 0x80);
-}
-
-// See if there's a link cable connected, if not, 0xff should be the response
-uint8_t test_link_connected() {
-    SB_REG = 0;
-    SC_REG = 0x81;
-    delay(5);
-    uint8_t disconnected = (SB_REG == 0xff);
-    SC_REG = 0;
-    return !disconnected;
+    while ((SC_REG & 0x80) && (++guard < LINK_CYCLES_MAX));
+    return (SB_REG != 0xff);
 }
 
 #define ID_DPAD 0
@@ -26,16 +21,18 @@ uint8_t test_link_connected() {
 
 #define PARITY(A) (((A) ^ ((A) >> 1) ^ ((A) >> 2) ^ ((A) >> 3)) & 0x01)
 
-void send_joypad(uint8_t j, uint8_t id) {
-    send_data((j) | ((PARITY(j)) ? (uint8_t) 0x90 : (uint8_t) 0x80) | id);
+uint8_t send_joypad(uint8_t j, uint8_t id) {
+    return send_data((j) | ((PARITY(j)) ? (uint8_t) 0x90 : (uint8_t)0x80) | id);
 }
 
-void send_ir_shutter() {
+uint8_t send_ir_shutter() {
     clock_t end_t = clock() + CLOCKS_PER_SEC / 4;
     do {
         RP_REG = ~RP_REG & 0x1;
         delay(10);
     } while (clock() < end_t);
+    RP_REG = 0;
+    return TRUE;
 }
 
 typedef struct submap_t {
@@ -69,45 +66,20 @@ void update_joy(uint8_t joy, const submap_t *coords) {
     }
 }
 
-void loop_serial() {
-    static uint8_t joy = 0, old_joy;
-    while (TRUE) {
-        old_joy = joy, joy = joypad();
-
-        if ((old_joy ^ joy) & 0x0f) {
-            send_joypad(joy & 0x0f, ID_DPAD), update_joy(joy & 0x0f, pad);
-        }
-        if ((old_joy ^ joy) & 0xf0) {
-            send_joypad(joy >> 4, ID_BUTTONS), update_joy(joy >> 4, btn);
-        }
-        wait_vbl_done();
-    }
-}
-
-void loop_ir() {
-    static uint8_t joy = 0, old_joy;
-    while (TRUE) {
-        old_joy = joy, joy = joypad();
-
-        if ((~old_joy & J_A) & (joy & J_A)) {
-            send_ir_shutter();
-        }
-        if ((old_joy ^ joy) & 0x0f) {
-            update_joy(joy & 0x0f, pad);
-        }
-        if ((old_joy ^ joy) & 0xf0) {
-            update_joy(joy >> 4, btn);
-        }
-        wait_vbl_done();
-    }
-
+void update_connected(uint8_t connected) {
+    set_bkg_submap(DEVICE_SCREEN_X_OFFSET + 2, DEVICE_SCREEN_Y_OFFSET + 4, 3, 2,
+                   connected ? remote_down_map : remote_map,
+                   (remote_WIDTH / remote_TILE_W));
 }
 
 void main(void) {
-    static uint8_t should_send_ir;
+    static uint8_t has_ir = FALSE;
 
     DISPLAY_OFF;
-    if (_cpu == CGB_TYPE) cgb_compatibility();
+    if (_cpu == CGB_TYPE) {
+        cgb_compatibility();
+        has_ir = TRUE;
+    }
     set_bkg_data(0, remote_TILE_COUNT, remote_tiles);
     set_bkg_data(remote_TILE_COUNT, remote_down_TILE_COUNT, remote_down_tiles);
     set_bkg_tiles(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET, remote_WIDTH / remote_TILE_W,
@@ -116,15 +88,33 @@ void main(void) {
     DISPLAY_ON;
 
     for (uint8_t i = 30; i != 0; i--) wait_vbl_done();
-    should_send_ir = (_cpu == CGB_TYPE) && !test_link_connected();
-    if (!should_send_ir) {
-        set_bkg_submap(DEVICE_SCREEN_X_OFFSET + 2, DEVICE_SCREEN_Y_OFFSET + 4, 3, 2, remote_down_map,
-                       (remote_WIDTH / remote_TILE_W));
+    // refresh link state
+    static uint8_t connected = 0;
+    connected = send_data(0);
+    update_connected(connected);
+
+    static uint8_t joy = 0, old_joy;
+    static uint8_t old_connected;
+    while (TRUE) {
+        old_joy = joy, joy = joypad();
+        old_connected = connected;
+
+        if ((old_joy ^ joy) & 0x0f) {
+            connected = send_joypad(joy & 0x0f, ID_DPAD);
+            update_joy(joy & 0x0f, pad);
+        }
+        if ((old_joy ^ joy) & 0xf0) {
+            connected = send_joypad(joy >> 4, ID_BUTTONS);
+            update_joy(joy >> 4, btn);
+
+            if (has_ir && !connected && (joy & J_A)) {
+                send_ir_shutter();
+            }
+        }
+        if (old_connected ^ connected) {
+            update_connected(connected);
+        }
+        wait_vbl_done();
     }
 
-    if (should_send_ir) {
-        loop_ir();
-    } else {
-        loop_serial();
-    }
 }
