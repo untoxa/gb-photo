a0b92d22ce562c1eb53e15fc2fe2a418e2b67af0
Roger Braunstein <roger@partlyhuman.com>
2022-11-24 17:27:04 -0800

    Cleanup

--- remote/src/main.c
+++ remote/src/main.c
@@ -5,14 +5,12 @@
 #include "remote.h"
 #include "remote_down.h"
 
-#define LINK_CYCLES_MAX 0x200
-
-// If link cable disconnected, return false
-inline uint8_t send_data(uint8_t b) {
+uint8_t send_data(uint8_t b) {
     uint16_t guard = 0;
     SB_REG = b;
     SC_REG = 0x81;
-    while ((SC_REG & 0x80) && (++guard < LINK_CYCLES_MAX));
+    while (SC_REG & 0x80);
+    // Pulled high when link cable disconnected
     return (SB_REG != 0xff);
 }
 
@@ -22,7 +20,7 @@ inline uint8_t send_data(uint8_t b) {
 #define PARITY(A) (((A) ^ ((A) >> 1) ^ ((A) >> 2) ^ ((A) >> 3)) & 0x01)
 
 uint8_t send_joypad(uint8_t j, uint8_t id) {
-    return send_data((j) | ((PARITY(j)) ? (uint8_t) 0x90 : (uint8_t)0x80) | id);
+    return send_data((j) | ((PARITY(j)) ? (uint8_t) 0x90 : (uint8_t) 0x80) | id);
 }
 
 uint8_t send_ir_shutter() {
@@ -37,7 +35,7 @@ uint8_t send_ir_shutter() {
 
 typedef struct submap_t {
     uint8_t x, y, w, h;
-    const struct submap_t *next;
+    const struct submap_t * next;
 } submap_t;
 
 const submap_t pad[] = {
@@ -54,14 +52,12 @@ const submap_t btn[] = {
         {0x09, 0x0f, 3, 2, NULL}         // START
 };
 
-void update_joy(uint8_t joy, const submap_t *coords) {
-    for (const submap_t *b = coords; (b); b = b->next, joy >>= 1) {
+void update_joy(uint8_t joy, const submap_t * coords) {
+    for (const submap_t * b = coords; (b); b = b->next, joy >>= 1) {
         if (joy & 0x01) {
-            set_bkg_submap(DEVICE_SCREEN_X_OFFSET + b->x, DEVICE_SCREEN_Y_OFFSET + b->y, b->w, b->h, remote_down_map,
-                           (remote_down_WIDTH / remote_down_TILE_W));
+            set_bkg_submap(DEVICE_SCREEN_X_OFFSET + b->x, DEVICE_SCREEN_Y_OFFSET + b->y, b->w, b->h, remote_down_map, (remote_down_WIDTH / remote_down_TILE_W));
         } else {
-            set_bkg_submap(DEVICE_SCREEN_X_OFFSET + b->x, DEVICE_SCREEN_Y_OFFSET + b->y, b->w, b->h, remote_map,
-                           (remote_WIDTH / remote_TILE_W));
+            set_bkg_submap(DEVICE_SCREEN_X_OFFSET + b->x, DEVICE_SCREEN_Y_OFFSET + b->y, b->w, b->h, remote_map, (remote_WIDTH / remote_TILE_W));
         }
     }
 }
@@ -82,8 +78,7 @@ void main(void) {
     }
     set_bkg_data(0, remote_TILE_COUNT, remote_tiles);
     set_bkg_data(remote_TILE_COUNT, remote_down_TILE_COUNT, remote_down_tiles);
-    set_bkg_tiles(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET, remote_WIDTH / remote_TILE_W,
-                  remote_HEIGHT / remote_TILE_H, remote_map);
+    set_bkg_tiles(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET, remote_WIDTH / remote_TILE_W, remote_HEIGHT / remote_TILE_H, remote_map);
     SHOW_BKG;
     DISPLAY_ON;
 
@@ -100,12 +95,12 @@ void main(void) {
         old_connected = connected;
 
         if ((old_joy ^ joy) & 0x0f) {
-            connected = send_joypad(joy & 0x0f, ID_DPAD);
             update_joy(joy & 0x0f, pad);
+            connected = send_joypad(joy & 0x0f, ID_DPAD);
         }
         if ((old_joy ^ joy) & 0xf0) {
-            connected = send_joypad(joy >> 4, ID_BUTTONS);
             update_joy(joy >> 4, btn);
+            connected = send_joypad(joy >> 4, ID_BUTTONS);
 
             if (has_ir && !connected && (joy & J_A)) {
                 send_ir_shutter();
