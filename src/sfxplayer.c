1940ab1fa5762efeb6f93ab5c08dc1ff1f7cd7fd
Toxa <untoxa@mail.ru>
2023-02-19 20:43:50 +0300

    enhance sample playback

--- src/sfxplayer.c
+++ src/sfxplayer.c
@@ -68,19 +68,22 @@ lbl:
         cp #7
         jr z, 5$                    ; terminator
 
+        ldh a, (_NR51_REG)
+        ld c, a
+        and #0b10111011
+        ldh (_NR51_REG), a
+
         xor a
         ld (_NR30_REG), a
 
-        ld c, #__AUD3WAVERAM
-        .rept 16
+        .irp ofs,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
             ld a, (hl+)
-            ldh (c), a
-            inc c
+            ldh (__AUD3WAVERAM+ofs), a
         .endm
 
         ld a, b
         cp #6
-        jr nz, 4$                   ; just load waveform, not play
+        jr nz, 9$                   ; just load waveform, not play
 
         ld a, #0x80
         ldh (_NR30_REG),a
@@ -92,6 +95,10 @@ lbl:
         ldh (_NR33_REG),a
         ld a, #0xC7                 ; start; no loop; high freq bits are 111
         ldh (_NR34_REG),a
+
+9$:
+        ld a, c
+        ldh (_NR51_REG), a
 
         jr 4$
 5$:                                 ; terminator
