939487f8e743fbb4c07b293e8c5adcc7b33ce852
Toxa <untoxa@mail.ru>
2025-07-06 23:09:07 +0300

    fix misc asset location on the game boy targets

--- src/misc_assets.c
+++ src/misc_assets.c
@@ -12,8 +12,14 @@
 BANKREF(module_misc_assets)
 
 uint8_t INIT_module_misc_assets(void) BANKED {
+#if defined(NINTENDO)
+    set_banked_data(TO_TILE_ADDRESS(TILE_BANK_1, (0x100 - common_tiles_TILE_COUNT)), common_tiles_tiles, (common_tiles_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(common_tiles));
+    set_banked_data(TO_TILE_ADDRESS(TILE_BANK_1, (0x80 - cursors_TILE_COUNT)), cursors_tiles, (cursors_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(cursors));
+    set_banked_data(TO_TILE_ADDRESS(TILE_BANK_1, (0x80 - cursors_TILE_COUNT - hand_cursor_TILE_COUNT)), hand_cursor_tiles, (hand_cursor_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(hand_cursor));
+#elif defined(SEGA)
     set_banked_data(TO_TILE_ADDRESS(TILE_BANK_0, (0x100 - common_tiles_TILE_COUNT)), common_tiles_tiles, (common_tiles_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(common_tiles));
     set_banked_data(TO_TILE_ADDRESS(TILE_BANK_0, (0x80 - cursors_TILE_COUNT)), cursors_tiles, (cursors_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(cursors));
     set_banked_data(TO_TILE_ADDRESS(TILE_BANK_0, (0x80 - cursors_TILE_COUNT - hand_cursor_TILE_COUNT)), hand_cursor_tiles, (hand_cursor_TILE_COUNT << DEVICE_TILE_SIZE_BITS), BANK(hand_cursor));
+#endif
     return 0;
 }
